691d82cdc72ce7daae435a97485459c1598b2224
cdelira9 <cdelira@ucsd.edu>
2022-07-25 14:05:30 -0700

    location and new cateogry in vendors table

--- api/clothing-schema.sql
+++ api/clothing-schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE users (
     first_name  TEXT NOT NULL,
     last_name   TEXT NOT NULL,
     password    TEXT NOT NULL,
+    category    TEXT NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- ui/src/components/LocationSearch/LocationSearch.jsx
+++ ui/src/components/LocationSearch/LocationSearch.jsx
@@ -1,16 +1,58 @@
 import SearchIcon from "@mui/icons-material/Search";
 import "./LocationSearch.css";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 
 export default function LocationSearchBar(props) {
+  function geoFindMe() {
+    const status = document.querySelector("#status");
+    const mapLink = document.querySelector("#map-link");
+
+    mapLink.href = "";
+    mapLink.textContent = "";
+
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      status.textContent = "";
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+
+    function error() {
+      status.textContent = "Unable to retrieve your location";
+    }
+
+    if (!navigator.geolocation) {
+      status.textContent = "Geolocation is not supported by your browser";
+    } else {
+      status.textContent = "Locating…";
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+
+
   return (
     <div className="searchbar">
       <input
-        placeholder="Enter your zipcode"
+        placeholder="Enter zipcode"
         className="input"
         value={props.searchBar}
         onChange={(event) => props.handleOnSearchBarChange(event.target.value)}
       />
-      <SearchIcon className="search-icon" fontSize="large" />
+
+      <button id="find-me" onClick={geoFindMe}>
+        Click to use Location
+      </button>
+      <br />
+      <p id="status"></p>
+      <a id="map-link" target="_blank"></a>
+
+      {/* <button id="find-me">
+        <SearchIcon className="search-icon" fontSize="large" />
+      </button> */}
     </div>
   );
 }
