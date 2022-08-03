faf689f7b7f5e5dd250d900229b4ecf72b375791
Michelle Fox-Romero <michelle.fox@michell-ltmdryt.internal.salesforce.com>
2022-08-03 10:17:09 -0700

    fixed error in vendor navbar

--- ui/src/components/Store/StorePage.jsx
+++ ui/src/components/Store/StorePage.jsx
@@ -9,35 +9,30 @@ import StoreDetail from "./StoreDetail";
 export default function StorePage(props) {
   return (
     <div className="store-page">
-        <main>
-          <div className="banner">
-            <h1>Store</h1>
-          </div>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <StoreOverview
-                  store={props.store}
-                  user={props.user}
-                ></StoreOverview>
-              }
-            ></Route>
-            <Route
-              path="/create"
-              element={
-                <StoreNew addStore={props.addStore}></StoreNew>
-              }
-            ></Route>
-            <Route
-              path="/id/:storeId"
-              element={
-                <StoreDetail store={props.store}></StoreDetail>
-              }
-            ></Route>
-          </Routes>
-        </main>
-      
+      <main>
+        <div className="banner">
+          <h1>My Stores</h1>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <StoreOverview
+                store={props.store}
+                user={props.user}
+              ></StoreOverview>
+            }
+          ></Route>
+          <Route
+            path="/create"
+            element={<StoreNew addStore={props.addStore}></StoreNew>}
+          ></Route>
+          <Route
+            path="/id/:storeId"
+            element={<StoreDetail store={props.store}></StoreDetail>}
+          ></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
