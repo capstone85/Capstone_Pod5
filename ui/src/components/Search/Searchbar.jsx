06e6a3370c7f1e5d1ae082563283a4b61788f927
cdelira9 <cdelira@ucsd.edu>
2022-08-05 11:14:39 -0700

    add column

--- ui/src/components/Search/Searchbar.jsx
+++ ui/src/components/Search/Searchbar.jsx
@@ -1,7 +1,31 @@
 import "./Searchbar.css";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState, useEffect } from "react";
+import apiClient from "../../services/apiClient";
 
 export default function Searchbar(props) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsFetching(true);
+
+      const { data, error } = await apiClient.listAllProducts();
+      if (error) {
+        setError(error);
+      }
+      if (data) {
+        console.log("data", data.products);
+        setProduct(data.products);
+      }
+      setIsFetching(false);
+    };
+
+    fetchProducts();
+  }, []);
+
+  let foundSearch = false;
   return (
     <div className="searchbar-section">
       <div className="searchbar">
@@ -16,6 +40,40 @@ export default function Searchbar(props) {
         <div className="search-icon">
           <SearchIcon />
         </div>
+      </div>
+      <div className="product-grid">
+      {product.map((item, idx) => {
+        if (
+          item.name.toLowerCase().includes(props.searchbar.toLowerCase()) ||
+          item.category.toLowerCase().includes(props.searchbar.toLowerCase())
+        ) {
+          foundSearch = true;
+          return (
+            <ProductCard
+              key={idx}
+              category={item.category}
+              description={item.description}
+              // showDescription={false}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              // storeId={curr.id}
+              // products={props.products}
+              // quantity={quantity}
+              //  addToCart={() => addToCart(product)}
+              addToCart={props.handleAddItemToCart}
+              handleRemoveItemFromCart={props.handleRemoveItemFromCart}
+              setIsFetching={props.setIsFetching}
+              product={item}
+            />
+          );
+        }
+      })}
+      {!foundSearch ? (
+        <div className="none-found">
+          <h1>No products available.</h1>
+        </div>
+      ) : null}
       </div>
     </div>
   );
