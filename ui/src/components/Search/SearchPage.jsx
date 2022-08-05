ee35ad7a27a1226516483e1f8df3ec1b873708a2
cdelira9 <cdelira@ucsd.edu>
2022-08-05 11:47:54 -0700

    searchbar

--- ui/src/components/Search/SearchPage.jsx
+++ ui/src/components/Search/SearchPage.jsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import "./SearchPage.css";
 import SearchBar from "./Searchbar";
+import Selector from "../Product/Filter";
+import { useState } from "react";
 
 export default function SearchPage(props) {
+  // const [activeCategory, setActiveCategory] = useState("All Categories");
+  // const categories = ["All Categories", "clothing", "accessories", "footwear"];
   return (
     <>
       <h1 className="page-head">Search for stores or styles.</h1>
