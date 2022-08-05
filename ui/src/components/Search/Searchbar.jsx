1082319f2ee98094dabcd64ffa125d55907097e0
cdelira9 <cdelira@ucsd.edu>
2022-08-05 12:29:02 -0700

    search page work in progress

--- ui/src/components/Search/Searchbar.jsx
+++ ui/src/components/Search/Searchbar.jsx
@@ -6,36 +6,6 @@ import ProductCard from "../Product/ProductCard";
 import Selector from "../Product/Filter";
 
 export default function Searchbar(props) {
-  const [activeCategory, setActiveCategory] = useState("All Categories");
-  const categoriesList = [
-    "All Categories",
-    "clothing",
-    "accessories",
-    "footwear",
-  ];
-
-  const [isFetching, setIsFetching] = useState(false);
-  const [product, setProduct] = useState([]);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setIsFetching(true);
-
-      const { data, error } = await apiClient.listAllProducts();
-      if (error) {
-        setError(error);
-      }
-      if (data) {
-        console.log("data", data.products);
-        setProduct(data.products);
-      }
-      setIsFetching(false);
-    };
-
-    fetchProducts();
-  }, []);
-
-  let foundSearch = false;
   return (
     <div className="searchbar-section">
     <div className="searchbar">
@@ -50,6 +20,7 @@ export default function Searchbar(props) {
       <div className="search-icon">
         <SearchIcon />
       </div>
+      
     </div>
   </div>
   );
